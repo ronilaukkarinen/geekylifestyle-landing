bc64c9d07e58658b3ff4652c0a21dc5cd9a1ff5c
Roni Laukkarinen <roni@laukkarinen.info>
2017-03-14 18:14:24 +0200

    Add repo

---
+++ README.md
@@ -0,0 +1,9 @@
+# Geeky Lifestyle blog
+
+Started planning a new blog on Sunday, 12th of March, 2017. The logo and branding was ready on Monday, 13th of March, 2017.
+
+This repository is for backup purposes only.
+
+![Screenshot](https://geekylifestyle.com/landing.png "Screenshot")
+
+Created with [Modern HTML5 Boilerplate](https://github.com/ronilaukkarinen/modern-html5-boilerplate).
---
+++ dist/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Geeky lifestyle</title>
+        <meta name="description" content="Blog for geeky-minded and tech diggers. Opening soon!">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/global.css">
+        <link rel="shortcut icon" href="images/favicon.png">
+    </head>
+    <body>
+
+    <div class="wrapper">
+
+      <div class="slide slide-hero">
+        <div class="container">
+          <img src="images/logo-sorta.png" alt="Geeky Lifestyle -logo icon" />
+          <h1><span class="screen-reader-text">Geeky Lifestyle</span><?php echo file_get_contents( 'svg/logo.svg' ); ?></h1>
+          <p>New blog for geeky-minded and tech diggers. Coming soon!</p>
+
+          <!-- Begin MailChimp Signup Form -->
+          <form action="//geekylifestyle.us15.list-manage.com/subscribe/post?u=75ee5160a187ff4d1009816b1&amp;id=5526ade866" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+              <div id="mc_embed_signup_scroll">
+
+                <div id="mce-responses" class="clear">
+              		<div class="response" id="mce-error-response" style="display:none"></div>
+              		<div class="response" id="mce-success-response" style="display:none"></div>
+              	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+
+                 <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_75ee5160a187ff4d1009816b1_5526ade866" tabindex="-1" value=""></div>
+
+          	     <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="To be first to know, type your e-mail here.">
+
+                 <input type="submit" value="Take me to the future" name="subscribe" id="mc-embedded-subscribe" class="button">
+              </div>
+          </form>
+          </div>
+          <!--End mc_embed_signup-->
+        </div><!-- .container -->
+      </div><!-- .slide -->
+
+    </div><!-- .wrapper -->
+
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-93498917-1', 'auto');
+          ga('send', 'pageview');
+
+        </script>
+
+    </body>
+</html>
